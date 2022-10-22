e9b73ebfb2c642d6dacb39e0dfdaea2c2230d086
术生何 <urfdvw@gmail.com>
2022-10-22 15:38:59 -0400

    update examples

--- examples/keypadi2c_single_expander.py
+++ examples/keypadi2c_single_expander.py
@@ -7,19 +7,14 @@ import busio
 # Adafruit
 from adafruit_mcp230xx.mcp23017 import MCP23017
 # My
-from keypadi2c import I2CKeyPad, I2CKeys
+from keypadi2c import I2CKeys
 
 #%% keypad defination
 # I2C
 SCL = board.SCL
 SDA = board.SDA
 i2c = busio.I2C(SCL, SDA, frequency=int(1e6))
-keypad = I2CKeyPad([
-    I2CKeys(mcp=MCP23017(i2c, 0x20), nbits=16, code_shift=0),
-    I2CKeys(mcp=MCP23017(i2c, 0x21), nbits=16, code_shift=16),
-    I2CKeys(mcp=MCP23017(i2c, 0x22), nbits=16, code_shift=32),
-    I2CKeys(mcp=MCP23017(i2c, 0x23), nbits=16, code_shift=48),
-])
+keypad = I2CKeys(mcp=MCP23017(i2c, 0x20), nbits=16)
 
 #%% main
 while True:
